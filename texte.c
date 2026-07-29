80ef1ff1526dc84c0dd1d0b137d32f945ba98a69
Paulo Cezar Freire Pimenta <87717926+paulinho-pimenta@users.noreply.github.com>
2026-07-29 14:26:09 -0300

    Update texte.c

--- texte.c
+++ texte.c
@@ -6,34 +6,52 @@ void limpar_tela(){
 }
 
 void esvaziar_matriz(char matriz[3][3]){
-    int i,j;
+    int i, j;
 
-    for(i = 0; i <= 2; i++){
-        for(j = 0; j <= 2; j++){
+    for (i = 0; i <= 2; i++)
+    {
+        for (j = 0; j <= 2; j++)
+        {
             matriz[i][j] = ' ';
-        }    
+        }
     }
 }
 
+int verifica_matriz(char matriz[3][3]){
+    int i, j;
+
+    for (i = 0; i <= 2; i++){
+        // horizontal
+        if (matriz[i][0] == matriz[i][1] && matriz[i][1] == matriz[i][2]){
+            return 1;
+        }
+
+        for (j = 0; j <= 2; j++){
+
+        }
+    }
+    return 0;
+}
+
 void preenche_matriz(char matriz[3][3]){
-    int i,j,k = 0,verificar = 0;
+    int i, j, k = 0;
     char simbolo;
 
     printf("Escolha o seu simbolo: X ou O\n");
-    scanf("%c",&simbolo);
+    scanf("%c", &simbolo);
 
-    while(verificar){
-        verificar = verifica_matriz;
+    while (k <= 9){
+        
         limpar_tela();
-        printf("Jogada atual: %c\n",simbolo);
+        printf("Jogada atual: %c\n", simbolo);
         mostrar_matriz(matriz);
         printf("De as coordenadas de onde ira jogar:\n");
-        scanf("%d %d",&i,&j);
+        scanf("%d %d", &i, &j);
 
-        if(simbolo == 'X' || simbolo == 'x'){
-           matriz[i][j] = simbolo;
-           simbolo = 'O';
-        }else if(simbolo == 'o' || simbolo == 'O'){
+        if (simbolo == 'X' || simbolo == 'x'){
+            matriz[i][j] = simbolo;
+            simbolo = 'O';
+        }else if (simbolo == 'o' || simbolo == 'O'){
             matriz[i][j] = simbolo;
             simbolo = 'X';
         }else{
@@ -41,33 +59,19 @@ void preenche_matriz(char matriz[3][3]){
         }
         k++;
     }
-
 }
 
 void mostrar_matriz(char matriz[3][3]){
-    int i,j;
-    for(i = 0; i <= 2; i++){
-        for(j = 0; j <= 2; j++){
-            printf(" %c | ",matriz[i][j]);
-        }    
+    int i, j;
+    for (i = 0; i <= 2; i++){
+        for (j = 0; j <= 2; j++){
+            printf("%c", matriz[i][j]);
+            if (j < 2){
+                printf(" | ");
+            }
+        }
         printf("\n");
     }
-}
-
-int verifica_matriz(char matriz[3][3]){
-    int i,j;
-
-    for(i = 0; i <= 2; i++){
-        //horizontal
-        if(matriz[i][0] == matriz[i][1] && matriz[i][1] == matriz[i][2]){
-            return 1;
-        }
-
-        for(j = 0; j <= 2; j++){
-            
-        }    
-    }
-    return 0;
 }
 
 int main(){
@@ -76,7 +80,7 @@ int main(){
     esvaziar_matriz(matriz);
     preenche_matriz(matriz);
     mostrar_matriz(matriz);
-    //verifica_matriz(matriz);
+    // verifica_matriz(matriz);
 
     return 0;
 }
