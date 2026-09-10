5fc459e9454e0b0e8f62ea0a70c59b23ba1775f4
Paulo Cezar Freire Pimenta <87717926+paulinho-pimenta@users.noreply.github.com>
2026-09-10 11:57:28 -0300

    Update texte.c

--- texte.c
+++ texte.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void limpar_tela();
+void esvaziar_matriz(char matriz[3][3]);
+int tabuleiro_cheio(char matriz[3][3]);
+char verifica_matriz(char matriz[3][3]);
+void preenche_matriz(char matriz[3][3]);
+void mostrar_matriz(char matriz[3][3]);
+
 void limpar_tela(){
-    system("cls");
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
 }
 
 void esvaziar_matriz(char matriz[3][3]){
@@ -15,21 +26,37 @@ void esvaziar_matriz(char matriz[3][3]){
     }
 }
 
-int verifica_matriz(char matriz[3][3], char vencedor, int *p_k){
+int tabuleiro_cheio(char matriz[3][3]){
     int i, j;
+    for(i = 0; i <= 2; i++)
+        for(j = 0; j <= 2; j++)
+            if(matriz[i][j] == ' ') return 0;
+    return 1;
+}
+
+char verifica_matriz(char matriz[3][3]){
+    int i;
     for(i = 0; i <= 2; i++){
-        if(matriz[i][0] != ' ' || matriz[i][1] != ' ' || matriz[i][2] != ' '){
-            if(matriz[i][0] == matriz[i][1] && matriz[i][1] == matriz[i][2]){
-                printf("O vencedor eh %c\n",vencedor); 
-                *p_k = 1;     
-            }else if(matriz[0][i] == matriz[1][i] && matriz[1][i] == matriz[2][i]){
-                printf("O vencedor eh %c\n",vencedor); 
-                *p_k = 1;  
-            }
+        //horizontal
+        if(matriz[i][0] != ' ' && matriz[i][0] == matriz[i][1] && matriz[i][1] == matriz[i][2]){
+            return matriz[i][0];
+        }
+        //vertical
+        if(matriz[0][i] != ' ' && matriz[0][i] == matriz[1][i] && matriz[1][i] == matriz[2][i]){
+            return matriz[0][i];
         }
     }
     
-    return 0;
+    //diagonal secundaria
+    if(matriz[0][0] != ' ' && matriz[0][0] == matriz[1][1] && matriz[1][1] == matriz[2][2]){
+        return matriz[0][0];
+    }
+    //diagonal
+    if(matriz[0][2] != ' ' && matriz[0][2] == matriz[1][1] && matriz[1][1] == matriz[2][0]){
+        return matriz[0][2];
+    }
+    
+    return ' ';
 }
 
 void preenche_matriz(char matriz[3][3]){
@@ -38,32 +65,61 @@ void preenche_matriz(char matriz[3][3]){
 
     printf("Escolha o seu simbolo: X ou O\n");
     scanf("%c", &simbolo);
+    while(simbolo != 'X' && simbolo != 'x' && simbolo != 'O' && simbolo != 'o'){
+        printf("Simbolo invalido! Escolha X ou O:\n");
+        while(getchar() != '\n'); // limpa o buffer
+        scanf("%c", &simbolo);
+    }
 
     while(k != 1){
         limpar_tela();
         printf("Jogada atual: %c\n", simbolo);
         mostrar_matriz(matriz);
         
+        //---jogadas---
         printf("De as coordenadas de onde vai jogar:\n");
-        scanf("%d %d", &i, &j);
+
+        //---tratando erros de entrada---
+        if(scanf("%d %d", &i, &j) != 2){
+            printf("Entrada invalida!\n");
+            while(getchar() != '\n'); // limpa o buffer
+            continue;
+        } 
+        if(i < 0 || i > 2 || j < 0 || j > 2){
+            printf("Coordenadas invalidas! Use valores de 0 a 2.\n");
+            continue;
+        }
+        if(matriz[i][j] != ' '){
+            printf("Essa posicao ja esta ocupada!\n");
+            continue;
+        }     
 
         limpar_tela();
         
         if(simbolo == 'X' || simbolo == 'x'){
             matriz[i][j] = 'X';  
-        }else if(simbolo == 'o' || simbolo == 'O'){
-            matriz[i][j] = 'O';
         }else{
-            printf("ERRO: jogada invalida ou execucao inesperada\n");
+            matriz[i][j] = 'O';
+        }       
+
+        //---empate---
+        char vencedor = verifica_matriz(matriz);
+        if(vencedor != ' '){
+            limpar_tela();
+            mostrar_matriz(matriz);
+            printf("O VENCEDOR EH: %c\n", vencedor);
+            k = 1;
         }
 
-        verifica_matriz(matriz,simbolo,&k);
-        if(simbolo == 'X' || simbolo == 'x'){
-            simbolo = 'O';
-        }else if(simbolo == 'o' || simbolo == 'O'){
-            simbolo = 'X';
+        if(k != 1 && tabuleiro_cheio(matriz)){
+            limpar_tela();
+            mostrar_matriz(matriz);
+            printf("EMPATE!\n");
+            k = 1;
         }
-        
+
+        //---troca de turno---
+        simbolo = (simbolo == 'X' || simbolo == 'x') ? 'O' : 'X';
     }
 }
 
@@ -85,7 +141,6 @@ int main(){
 
     esvaziar_matriz(matriz);
     preenche_matriz(matriz);
-    mostrar_matriz(matriz);
 
     return 0;
 }
